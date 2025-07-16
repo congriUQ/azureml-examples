48b6e5b9a5fefd7f6a44bfcf352724305d37fbea
constantin <constantin.grigo@procon-it.de>
2025-07-16 13:26:54 +0200

    fixing training job

--- train.py
+++ train.py
@@ -1,21 +1,11 @@
-from azure.ai.ml import MLClient
-from azure.identity import DefaultAzureCredential
-import pandas as pd
+import mltable
+import argparse
 
-# Create ML client (works in local dev; in Azure job use managed identity)
-ml_client = MLClient(
-    credential=DefaultAzureCredential(),
-    subscription_id=os.environ["AZURE_SUBSCRIPTION_ID"],
-    resource_group_name=os.environ["AZURE_RESOURCE_GROUP"],
-    workspace_name=os.environ["AZURE_WORKSPACE"],
-)
+parser = argparse.ArgumentParser()
+parser.add_argument("--training_data", type=str)
+args = parser.parse_args()
 
-# Get registered dataset (Asset) by name
-data_asset = ml_client.data.get(name="my_registered_dataset", version="1")
-
-# Load dataset file path
-data_path = data_asset.path
-
-# Example: if it’s a CSV
-df = pd.read_csv(data_path)
+# Load MLTable dataset
+tbl = mltable.load(args.training_data)
+df = tbl.to_pandas_dataframe()
 print(df.head())
