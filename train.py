0c3adc32261c31341b66b4f3ffaa456d3cf9f0bd
constantin <constantin.grigo@procon-it.de>
2025-07-16 19:25:05 +0200

    debugging

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ args = parser.parse_args()
 
 print(f"args\n\n{args}")
 
-# # Load MLTable dataset
-# tbl = mltable.load(args.training_data)
-# df = tbl.to_pandas_dataframe()
-# print(df.head())
+# Load MLTable dataset
+tbl = mltable.load(args.training_data)
+df = tbl.to_pandas_dataframe()
+print(df.head())
