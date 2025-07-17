f5ecf5c3ef5bc24472c9d34be7cdc3347d1b9a8d
constantin <constantin.grigo@procon-it.de>
2025-07-17 09:01:07 +0200

    saving model to registry

--- train.py
+++ train.py
@@ -4,6 +4,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
+from azure.ai.ml import MLClient
+from azure.identity import DefaultAzureCredential
+from azure.ai.ml.entities import Model
+from pickle import dump
 
 
 parser = argparse.ArgumentParser()
@@ -31,6 +35,25 @@ x_test = scaler.transform(x_test)
 clf = LogisticRegression()
 clf.fit(x_train, y_train)
 
+from pickle import dump
+with open("model.pkl", "wb") as f:
+    dump(clf, f, protocol=5)
+
 # Evaluate
 y_pred = clf.predict(x_test)
 print(classification_report(y_test, y_pred))
+
+ml_client = MLClient(
+    DefaultAzureCredential(),
+    subscription_id="your-subscription-id",
+    resource_group_name="your-resource-group",
+    workspace_name="your-workspace"
+)
+
+model = Model(
+    path="./model.pkl",   # Path to model file
+    name="logistic_regression",       # Model name
+    description="A sample logistic regression model for the Diabetes dataset",
+    tags={"type": "logistic_regression"},
+    type="mlflow_model"         # Or 'custom_model', 'triton_model' etc.
+)
