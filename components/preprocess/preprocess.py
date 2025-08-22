e9faa9392b13d81a5e09519cfd4536b1835d432c
constantin.grigo <constantin.grigo@gmail.com>
2025-08-22 09:20:19 +0200

    data handover from preprocess

--- components/preprocess/preprocess.py
+++ components/preprocess/preprocess.py
@@ -3,6 +3,7 @@ import argparse
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from pathlib import Path
+import numpy as np
 
 
 parser = argparse.ArgumentParser()
@@ -28,6 +29,5 @@ scaler = StandardScaler()
 x_train = scaler.fit_transform(x_train)
 x_test = scaler.transform(x_test)
 
-x_train.to_parquet(Path(args.training_data) / "train.parquet")
-x_test.to_parquet(Path(args.test_data) / "test.parquet")
-
+np.save(Path(args.training_data) / "x_train.npy", x_train)
+np.save(Path(args.test_data) / "x_test.npy", x_test)
