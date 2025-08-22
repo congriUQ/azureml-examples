cd13ff14128019e6ca763219c18aa89c310181c1
constantin.grigo <constantin.grigo@gmail.com>
2025-08-22 11:54:21 +0200

    data handover from preprocess

--- components/train/train.py
+++ components/train/train.py
@@ -3,6 +3,10 @@ from pathlib import Path
 from uuid import uuid4
 from datetime import datetime
 import os
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from pickle import dump
+
 
 parser = argparse.ArgumentParser("train")
 parser.add_argument("--training_data", type=str, help="Path to training data")
@@ -30,14 +34,17 @@ print("mounted_path files: ")
 arr = os.listdir(args.training_data)
 print(arr)
 
-for filename in arr:
-    print("reading file: %s ..." % filename)
-    with open(os.path.join(args.training_data, filename), "r") as handle:
-        print(handle.read())
+x_train = np.load(Path(args.training_data) / "x_train.npy")
+y_train = np.load(Path(args.training_data) / "y_train.npy")
+
+clf = LogisticRegression()
+clf.fit(x_train, y_train)
+
+with open(Path(args.model_output) / "model.pkl", "wb") as model_file:
+    dump(clf, model_file, protocol=5)
 
 
 # Do the train and save the trained model as a file into the output folder.
 # Here only output a dummy data for demo.
 curtime = datetime.now().strftime("%b-%d-%Y %H:%M:%S")
 model = f"This is a dummy model with id: {str(uuid4())} generated at: {curtime}\n"
-(Path(args.model_output) / "model.txt").write_text(model)
