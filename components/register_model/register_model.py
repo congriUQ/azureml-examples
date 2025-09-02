31c931755411110c554f5a4dceade16da2351622
Christian Heynck <christian.heynck@procon-it.de>
2025-09-02 16:27:00 +0200

    model

--- components/register_model/register_model.py
+++ components/register_model/register_model.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 import mlflow
 from azure.ai.ml import MLClient
-from azure.ai.ml.entities import Model, Input
+from azure.ai.ml.entities import Model
 from azure.identity import ManagedIdentityCredential
 import sklearn
 
@@ -57,7 +57,7 @@ lineage_path = f"azureml://jobs/{parent_job_id}/outputs/model_output/"
 print(lineage_path)
 
 model = Model(
-    path=Input(args.model, type="mlflow_model"),
+    path=args.model,
     name="logistic_regression",
     type="mlflow_model",
     description="A sample logistic regression model for the Diabetes dataset",
